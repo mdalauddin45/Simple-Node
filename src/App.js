87fec08dc398e0098ef064fac157e500e493220d
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-29 11:29:37 +0600

    dkj

--- src/App.js
+++ src/App.js
@@ -2,40 +2,54 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [user, setUser] = useState([]);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/user")
+    fetch("http://localhost:5000/users")
       .then((res) => res.json())
-      .then((data) => setUser(data));
+      .then((data) => setUsers(data));
   }, []);
 
-  const handleAddUser = (e) => {
-    e.preventDefault();
-    const name = e.target.name.value;
-    const email = e.target.email.value;
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
     const user = { name, email };
     console.log(user);
-    e.target.reset();
+
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const newUsers = [...users, data];
+        setUsers(newUsers);
+      })
+      .catch((err) => console.error(err));
+
+    event.target.reset();
   };
+
   return (
     <div className="App">
       <form onSubmit={handleAddUser}>
-        <input type="text" name="name" id="" placeholder="Enter your name" />{" "}
+        <input type="text" name="name" placeholder="Name" />
         <br />
-        <input
-          type="email"
-          name="email"
-          id=""
-          placeholder="Enter your email"
-        />{" "}
+        <input type="email" name="email" id="" placeholder="Email" />
         <br />
-        <button type="submit"> Add user</button>
+        <button type="submit">Add User</button>
       </form>
-      <h1>user:{user.length}</h1>
+
+      <h2>Users: {users.length}</h2>
       <div>
-        {user.map((user) => (
-          <p key={user.id}>
+        {users.map((user) => (
+          <p key={user._id}>
+            {" "}
             {user.name} {user.email}{" "}
           </p>
         ))}
